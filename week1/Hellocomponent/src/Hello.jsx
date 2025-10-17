50b0ccfd9aeba3740cfb857964f9d8a53c91a241
Oyedoyin Samuel <sammmie225@gmail.com>
2025-10-17 14:34:01 +0100

    M

---
+++ week1/Hellocomponent/src/Hello.jsx
@@ -0,0 +1,11 @@
+
+function Hello(){
+    return(
+    <div className="main">
+    <h1>SAMUEL OYEDOYIN</h1>
+    <p>Abeokuta, NIgeria.</p>
+    <p>Welcome To My Journey As An AI  Developer</p>
+    </div>
+    );
+}
+export default Hello;
